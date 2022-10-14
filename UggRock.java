51b9176c05c78ee58ce8988554df2d44094951d1
Jude <92600970+CheeseAndHamBagel@users.noreply.github.com>
2022-10-14 14:20:29 +0100

    Add files via upload

--- UggRock.java
+++ UggRock.java
@@ -22,19 +22,46 @@ public class UggRock{
     }
 
     public int CalcStateValue (UggRockSize rockSize, UggRockType rockType) {
-        public int workingStateValue = 0;
+        int workingStateValue = 0;
         switch (rockSize) {
-        case UggRockSize.bigun:
+        case bigun:
         workingStateValue += 5;
         break;
         
-        case UggRockSize.quite_bigun:
+        case quite_bigun:
         workingStateValue += 3;
         break;
         
-        case UggRockSize.not_so_bigun:
+        case not_so_bigun:
         workingStateValue += 1;
         break;
         }
+
+        switch (rockType) {
+        case speckily:
+        workingStateValue += 7;
+        break;
+        
+        case ouchy_black:
+        workingStateValue += 20;
+        break;
+        
+        case floaty:
+        workingStateValue += 2;
+        break;
+
+        case hot_hot_hot:
+        workingStateValue += 15;
+        break;
+        }
+        return workingStateValue;
     }
+
+    public int getVal (){
+        int value = CalcStateValue(self.rockSize, self.rockType);
+        return value; 
+    }
+
+UggRock Terry = new UggRock (UggRockType.speckily, UggRockSize.quite_bigun);
+
 }
